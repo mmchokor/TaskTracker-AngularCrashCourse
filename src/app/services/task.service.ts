571ffca6e5338dedd356269f07c01587bd2ed056
mmchokor <mmchokor@gmail.com>
2023-10-20 14:19:44 +0300

    Submit & Create Task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -29,4 +29,8 @@ export class TaskService {
   updateTaskReminder(task: Task): Observable<Task> {
     return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task, httpOptions);
   }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
+  }
 }
