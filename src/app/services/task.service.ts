50968547546cf1f8fe33efddb1288ae6b3716a9f
mmchokor <mmchokor@gmail.com>
2023-10-20 13:55:43 +0300

    toggle reminder

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,6 +4,12 @@ import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
 @Injectable({
   providedIn: 'root',
 })
@@ -18,5 +24,9 @@ export class TaskService {
 
   deleteTask(task: Task): Observable<Task> {
     return this.http.delete<Task>(`${this.apiUrl}/${task.id}`);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task, httpOptions);
   }
 }
